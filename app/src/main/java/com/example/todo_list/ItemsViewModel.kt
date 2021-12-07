68d8932bd33c313a8383d46cc8ab9720213d0b9e
iqbalriiaz <iqbalriiaz@gmail.com>
2021-12-07 19:17:16 +0600

    splash screen && recycler view added

---
+++ app/src/main/java/com/example/todo_list/ItemsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.todo_list
+
+data class ItemsViewModel(val image: Int, val text: String) {
+}
---
+++ app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.todo_list
+
+import CustomAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // This is used to hide the status bar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.hide() // this will hide the action bar
+
+
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModel(R.drawable.ic_baseline_folder_24, "Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
+
+
+
+
+
+    }
+}
